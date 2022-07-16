a3dddc228b75bdf333b8f1b65ff2aebee196717c
Mériadec Lantonnet <meriadeclantonnet@gmail.com>
2022-07-16 14:15:54 +0200

    Add `Demo` configuration

---
+++ src/DemoMain.cpp
@@ -0,0 +1,71 @@
+#include "SSS/GL.hpp"
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_KP_0 || key == GLFW_KEY_ESCAPE) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main() try
+{
+    using namespace SSS;
+
+    // Create Window
+    GL::Window::CreateArgs args;
+    args.title = "SSS/Text-Rendering - Demo Window";
+    args.w = static_cast<int>(600);
+    args.h = static_cast<int>(600);
+    GL::Window::Shared window = GL::Window::create(args);
+
+    // Set context
+    GL::Context const context(window);
+
+    // Finish setting up window
+    glClearColor(0.3f, 0.3f, 0.3f, 0.f);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    window->setVSYNC(true);
+    window->setCallback(glfwSetKeyCallback, key_callback);
+
+    // SSS/GL objects
+
+    // Create objects
+    auto const& texture = GL::Texture::create();
+    auto const& camera = GL::Camera::create();
+    auto const& plane = GL::Plane::create();
+    auto const& plane_renderer = GL::Plane::Renderer::create();
+
+    // Text
+    auto const& area = TR::Area::create(300, 300);
+    auto fmt = area->getFormat();
+    fmt.style.charsize = 50;
+    fmt.color.text.func = TR::Format::Color::Func::rainbow;
+    area->setFormat(fmt);
+    area->parseString("Lorem ipsum dolor sit amet.");
+    texture->setTextAreaID(area->getID());
+    texture->setType(GL::Texture::Type::Text);
+
+    // Camera
+    camera->setPosition({ 0, 0, 3 });
+    camera->setProjectionType(GL::Camera::Projection::OrthoFixed);
+
+    // Plane
+    plane->setTextureID(texture->getID());
+    plane->scale(glm::vec3(300));
+    plane_renderer->chunks.emplace_back();
+    plane_renderer->chunks[0].reset_depth_before = true;
+    plane_renderer->chunks[0].objects.push_back(0);
+
+    // Main loop
+    while (!window->shouldClose()) {
+        // Poll events, threads, etc
+        GL::pollEverything();
+        // Draw renderers
+        window->drawObjects();
+        // Swap buffers
+        window->printFrame();
+    }
+}
+CATCH_AND_LOG_FUNC_EXC;
